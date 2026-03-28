754163f17b4cbb5ba71216ad831a7319b6505d66
JeniaShishkin <shn.yevgeny@gmail.com>
2026-03-28 22:09:53 +0300

    SSO implementation fix

--- MyString.cpp
+++ MyString.cpp
@@ -6,22 +6,22 @@ MyString::MyString(const char* str)
     if (length <= SHORT_STR_LENGTH) // String is SSO
     {
         std::memcpy(m_data.shortString, str, length);  
-        m_data.shortString[SHORT_STR_LENGTH] = length | SSO_MASK;
+        m_data.shortString[SHORT_STR_LENGTH] = static_cast<unsigned char>(length) | SSO_MASK; // SSO_MASK is unsigned char, therefore we cast the length to same type.
     } 
 	else // String is a long string.
 	{ 
         m_data.longString.longStringPtr = new char[length + 1];
         std::memcpy(m_data.longString.longStringPtr, str, length + 1);
-        m_data.longString.size = length; // MSB 0 = heap
+        m_data.longString.size = length; // Assumed MSB will always be zero, unless strlen > 2^54 which is absurd.
     }
 }
 
 MyString::MyString(const MyString& other)
 {
-	size_t length = other.m_data.longString.size;
-    if (other.isSSO())
+	size_t length = other.size();
+	if (other.isSSO())
 	{
-        std::memcpy(m_data.shortString, other.m_data.shortString, length + 1);
+		std::memcpy(m_data.shortString, other.m_data.shortString, length + 1);
     }
 	else
 	{
@@ -35,7 +35,9 @@ MyString::MyString(MyString&& other) noexcept
 {
     if (other.isSSO()) 
 	{
-        std::memcpy(m_data.shortString, other.m_data.shortString, 16);
+        std::memcpy(m_data.shortString, other.m_data.shortString, sizeof(SSO)); // Copying the entire 16 bytes, including metadata.
+		other.m_data.shortString[0] = '\0';
+		other.m_data.shortString[SHORT_STR_LENGTH] = SSO_MASK; // isSSO = true, size = 0.
     }
 	else
 	{
@@ -48,27 +50,27 @@ MyString::MyString(MyString&& other) noexcept
 std::strong_ordering MyString::operator<=>(const MyString& other) const noexcept 
 {
     size_t length = std::min(size(), other.size());
-    int cmp = std::memcmp(data(), other.data(), length);
+    int cmp = strcmp(data(), other.data());
     if (cmp != 0) return cmp < 0 ? std::strong_ordering::less : std::strong_ordering::greater;
-    return size() <=> other.size();
+    return size() <=> other.size(); // Size returns value that is not boolean, therefore used default spaceship operator between integers.
 }
 
 MyString MyString::operator+(const MyString& other) const {
     size_t totalLen = size() + other.size();
     MyString result;
-    if (totalLen <= 15) // SSO
+    if (totalLen <= SHORT_STR_LENGTH) // SSO
 	{
         std::memcpy(result.m_data.shortString, data(), size());
         std::memcpy(result.m_data.shortString + size(), other.data(), other.size());
         result.m_data.shortString[totalLen] = '\0';
-        result.m_data.shortString[15] = totalLen | SSO_MASK;
+        result.m_data.shortString[SHORT_STR_LENGTH] = static_cast<unsigned char>(totalLen) | SSO_MASK;
     }
 	else  // Long string
 	{ 
         char* newString = new char[totalLen + 1];
         std::memcpy(newString, data(), size());
         std::memcpy(newString + size(), other.data(), other.size());
-        // buf[totalLen] = '\0';
+        newString[totalLen] = '\0';
         result.m_data.longString.longStringPtr = newString;
         result.m_data.longString.size = totalLen;
     }
@@ -80,5 +82,5 @@ MyString MyString::operator+(const MyString& other) const {
 bool MyString::operator==(const MyString& other) const noexcept 
 {
     if (size() != other.size()) return false;
-    return std::memcmp(data(), other.data(), size()) == 0;
+    return strcmp(data(), other.data()) == 0;
 }
--- MyString.h
+++ MyString.h
@@ -17,7 +17,7 @@ public:
 
 	/* OPERATORS */
 	const char& operator[](size_t index) const { if (index >= size()) throw std::out_of_range("Index out of range"); return data()[index]; }
-	// char& operator[](size_t index) { if (index >= size()) throw std::out_of_range("Index out of range"); return data()[index]; }
+	char& operator[](size_t index) { if (index >= size()) throw std::out_of_range("Index out of range"); return data()[index]; }
 	std::strong_ordering operator<=>(const MyString& other) const noexcept;
 	bool operator==(const MyString& other) const noexcept;
 	MyString operator+(const MyString& other) const;
@@ -25,25 +25,27 @@ public:
     // friend std::ostream& operator<<(std::ostream& os, const MyString& s) { os << s.m_str; return os ; }
 
 	MyString& operator=(MyString other) { swap(other); return *this; }; // Supposed to be valid for both copy ans move.
-
-	// Old implementation of move/copy assignments.
-//	MyString& operator=(const MyString &other) { MyString copy{other}; swap(copy); return *this ; } // Copy assignment.
-//	MyString& operator=(MyString &&other) noexcept { swap(other); return *this; } // Move assignment.
-
+	
 	// Extra functions.
 	void swap(MyString &other) noexcept { using std::swap; swap(m_data, other.m_data); }
-	// SSO detection related functions.
-	bool isSSO() const noexcept { return (m_data.shortString[sizeof(SSO) - 1] & SSO_MASK) != 0; }	
-	size_t ssoSize() const noexcept { return m_data.shortString[sizeof(SSO) - 1] & SIZE_MASK; }
-	const char* data() const noexcept { return isSSO() ? m_data.shortString : m_data.longString.longStringPtr; }
 	size_t size() const noexcept { return isSSO() ? ssoSize() : m_data.longString.size; }
+	size_t ssoSize() const noexcept { return m_data.shortString[sizeof(SSO) - 1] & SIZE_MASK; }
+
+	bool isSSO() const noexcept { return (m_data.shortString[sizeof(SSO) - 1] & SSO_MASK) != 0; }
+
+	
+	// Functions to return pointer to data to support index-based access.
+	const char* data() const noexcept { return isSSO() ? m_data.shortString : m_data.longString.longStringPtr; }
+	char* data() noexcept { return isSSO() ? m_data.shortString : m_data.longString.longStringPtr; }
+	
+	
 
 	~MyString() { if (!isSSO() && m_data.longString.longStringPtr) delete[] m_data.longString.longStringPtr; }
 
 private:
 	static constexpr int SHORT_STR_LENGTH = 15;
-	static constexpr unsigned char SSO_MASK = 0x8000000000000000; // 1000 0000
-	static constexpr unsigned char SIZE_MASK = 0x7F00000000000000; // 0111 1111
+	static constexpr unsigned char SSO_MASK = 0x80; // 1000 0000
+	static constexpr unsigned char SIZE_MASK = 0x7F; // 0111 1111
 
 	union SSO
 	{
@@ -55,9 +57,6 @@ private:
 		} longString;
 		char shortString[sizeof(longString)];
 	};
-
-	static bool isSSO(char* shortString);
-	static bool isSSO(size_t length);
 	SSO m_data;
 };
 
